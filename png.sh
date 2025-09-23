79e5d467125568c9ad30ff75d48c9c65e27f9d4a
Anila Gokada <anila.gokada.87@gmail.com>
2025-09-23 16:17:54 +0530

    Update png.sh

--- png.sh
+++ png.sh
@@ -1,3 +1,4 @@
+kubectl create namespace monitoring #craete a namespace 
 # Prometheus repo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
@@ -8,13 +9,13 @@ helm repo update
 
 #install Prometheus
 helm install prometheus prometheus-community/prometheus \
-  --namespace <namespace-name> \
+  --namespace monitoring \  
   --set alertmanager.persistentVolume.enabled=true \
   --set server.persistentVolume.enabled=true
 
 #install Grafana
 helm install grafana grafana/grafana \
-  --namespace <namespace-name> \
+  --namespace monitoring \
   --set persistence.enabled=true \
   --set adminPassword='YourStrongPassword' \
   --set service.type=LoadBalancer
