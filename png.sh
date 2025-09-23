ed30a95c0646917db518655c237740668481cfab
Anila Gokada <anila.gokada.87@gmail.com>
2025-09-23 16:10:16 +0530

    Create png.sh

---
+++ png.sh
@@ -0,0 +1,20 @@
+# Prometheus repo
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+# Grafana repo
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+#install Prometheus
+helm install prometheus prometheus-community/prometheus \
+  --namespace <namespace-name> \
+  --set alertmanager.persistentVolume.enabled=true \
+  --set server.persistentVolume.enabled=true
+
+#install Grafana
+helm install grafana grafana/grafana \
+  --namespace <namespace-name> \
+  --set persistence.enabled=true \
+  --set adminPassword='YourStrongPassword' \
+  --set service.type=LoadBalancer
